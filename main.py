ef9f424ef589eb59d644a2f2e0a2bd91697f6fda
Zakhar <marinin.vsyakoe@gmail.com>
2026-06-11 22:15:44 +0600

    code update

--- main.py
+++ main.py
@@ -1,21 +1,37 @@
+import urllib.parse
+import webbrowser
+from datetime import datetime
 import speech_recognition as sr
 import pyttsx3
 from fuzzywuzzy import fuzz
 from num2words import num2words
+from ctypes import *
 
-def speak(text):
-    print(text)
+# Глушилка системных логов (только для Linux)
+try:
+    ERROR_HANDLER_FUNC = CFUNCTYPE(None, c_char_p, c_int, c_char_p, c_int, c_char_p)
+    def py_error_handler(filename, line, function, err, fmt):
+        pass
+    c_error_handler = ERROR_HANDLER_FUNC(py_error_handler)
+    asound = cdll.LoadLibrary('libasound.so.2')
+    asound.snd_lib_error_set_handler(c_error_handler)
+except OSError:
+    pass
 
-    engine = pyttsx3.init()
+class VoiceAssistant:
+    def __init__(self):
+        self.engine = pyttsx3.init()
 
-    voices = engine.getProperty('voices')
-    try:
-        engine.setProperty('voice', voices[0].id)
-    except IndexError:
-        engine.setProperty('voice', voices[1].id)
+        voices = self.engine.getProperty('voices')
+        try:
+            self.engine.setProperty('voice', voices[1].id)
+        except IndexError:
+            self.engine.setProperty('voice', voices[0].id)
 
-    engine.say(text)
-    engine.runAndWait()
+    def speak(self, text):
+        print(text)
+        self.engine.say(text)
+        self.engine.runAndWait()
 
 def command():
     r = sr.Recognizer()
@@ -36,10 +52,13 @@ def command():
             return command()
 
 opts = {
-    'name': ('jarvis', 'jarvees', 'deepseek'),
+    'name': ('jarvis', 'jarvees', 'deep seek'),
     'user_commands': ('tell', 'say', 'show', 'how', 'how many', 'search', 'me', 'please'),
     'cmds': {
         'time': ('time', 'what time', 'the time'),
+        'google': ('find', 'google'),
+        'rutube': ('rutube', ),
+        'youtube': ('youtube', ),
     }
 }
 
@@ -53,4 +72,69 @@ def recognize_cmd(cmd):
                 RC['percent'] = vrt
 
     return RC
-# Напишем по мере урока :)
+
+def youtube_search(query):
+    tes = urllib.parse.quote_plus(query)
+    url = f"https://www.youtube.com/results?search_query={tes}"
+    webbrowser.open(url, new=1)
+
+def rutube_search(query):
+    tes = urllib.parse.quote_plus(query)
+    url = f"https://rutube.ru/search/?query={tes}"
+    webbrowser.open(url, new=1)
+
+def google_search(query):
+    tes = urllib.parse.quote_plus(query)
+    url = f"https://www.google.com/search?q={tes}"
+    webbrowser.open(url, new=1)
+
+def exec_command(cmd_key, raw_cmd, assistant: VoiceAssistant):
+    if cmd_key == 'time':
+        now = datetime.now()
+        hours = num2words(now.hour)
+        minutes = num2words(now.minute)
+
+        print(f"[logs] user's command: {raw_cmd}")
+
+        assistant.speak(f"- It is {hours} hours and {minutes} minutes.")
+
+    elif cmd_key == 'google':
+        search_query = raw_cmd.replace('google', '').replace('find', '').strip()
+
+        assistant.speak('- The google search has been opened for you.')
+        google_search(search_query)
+
+    elif cmd_key == 'rutube':
+        search_query = raw_cmd.replace('rutube', '').replace('on', '')
+
+        assistant.speak('- The Rutube search has been opened for you.')
+        rutube_search(search_query)
+
+    elif cmd_key == 'youtube':
+        search_query = raw_cmd.replace('youtube', '').replace('on', '')
+
+        assistant.speak('- The youtube search has been opened for you.')
+        youtube_search(search_query)
+    else:
+        print("Not recognized! Repeat, please.")
+
+if __name__ == '__main__':
+    assistant = VoiceAssistant()
+
+    assistant.speak("- Hello! I am ready.")
+    while True:
+        user_input = command()
+
+        if user_input.startswith(opts['name']):
+            raw_cmd = user_input
+
+            for i in opts['name']:
+                raw_cmd = raw_cmd.replace(i, '', 1).strip()
+
+            for i in opts['user_commands']:
+                raw_cmd = raw_cmd.replace(i, '').strip()
+
+            cmd_res = recognize_cmd(raw_cmd)
+            exec_command(cmd_res['cmd'], raw_cmd, assistant)
+
+# Остальное напишем по мере урока :)
